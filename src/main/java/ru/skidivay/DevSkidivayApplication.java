2d3a77d569d66e95e6dd9197d9b1c04adeb9fac8
Masik <yaroshenko_andrey2006@mail.ru>
2025-11-01 00:55:55 +0700

    init microservices

---
+++ src/main/java/ru/skidivay/DevSkidivayApplication.java
@@ -0,0 +1,13 @@
+package ru.skidivay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSkidivayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSkidivayApplication.class, args);
+    }
+
+}
