16335725a0c3756dc807012a03d25b1405cdff00
Masik <yaroshenko_andrey2006@mail.ru>
2025-11-02 01:02:42 +0700

    init gateway application

---
+++ api-gateway/src/main/java/ru/skidivay/gateway/GatewayApplication.java
@@ -0,0 +1,11 @@
+package ru.skidivay.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayApplication.class, args);
+    }
+}
