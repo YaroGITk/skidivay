4aa090807d3b8a7201a5e73c9ec596321c497fb0
Masik <yaroshenko_andrey2006@mail.ru>
2025-11-02 01:34:12 +0700

    create gateway config

--- api-gateway/src/main/java/ru/skidivay/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/ru/skidivay/gateway/config/GatewayConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GateWayConfig {
+public class GatewayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
